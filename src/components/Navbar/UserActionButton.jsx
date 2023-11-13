35d8d706af4e72ab00172e61835eafceb15f4880
haris2303 <hrsccf102@gmail.com>
2023-11-13 14:43:35 +0900

    first

--- src/components/Navbar/UserActionButton.jsx
+++ src/components/Navbar/UserActionButton.jsx
@@ -7,9 +7,14 @@ const UserActionButton = async () => {
   const actionURL = user ? "signout" : "signin";
 
   return (
-    <div>
+    <div className="flex justify-between items-center gap-3">
       {!user ? null : <Link href="/user/dashboard">Dashboard</Link>}
-      <Link href={`/api/auth/${actionURL}`}>{actionLabel}</Link>
+      <Link
+        href={`/api/auth/${actionURL}`}
+        className="bg-color-dark text-color-accent py-2 px-3 rounded-lg"
+      >
+        {actionLabel}
+      </Link>
     </div>
   );
 };
